bf7293f4f849140b87b7b69d3935fbfbcde173f8
Paku Thao <thao2210@stthomas.edu>
2023-10-25 13:44:33 -0500

    commit

--- dynamic_server.mjs
+++ dynamic_server.mjs
@@ -12,7 +12,28 @@ const root = path.join(__dirname, 'public');
 const template = path.join(__dirname, 'templates');
 
 let app = express();
-app.use(express.static(root));
+app.use(express.static(template));
+
+const db = new sqlite3.Database(path.join(__dirname, 'fandango_score_comparison.sqlite3'), sqlite3.OPEN_READONLY, (err) => {
+    if(err) {
+        console.log('Error connecting to database.');
+    } else {
+        console.log('Successfully connected to database.');
+    }
+
+
+});
+
+app.get('/:letter', (req, res) => {
+    let letter = req.params.letter;
+    console.log(letter);
+    fs.readFile(path.join(root, 'index.html'), 'utf-8', (err, data) => {
+        if (err) {
+            res.status(404).type('txt').send('File Not Found.');
+        }
+        res.status(200).type('html').send(data);
+    });
+});
 
 app.listen(port, () => {
     console.log('Now listening on port ' + port);
