d7332977730f2d67947d963d0ac1d2134057825b
matt-lao <matthew.lao24@gmail.com>
2023-10-31 17:45:21 -0500

    init /film/:title route

--- dynamic_server.mjs
+++ dynamic_server.mjs
@@ -65,6 +65,30 @@ app.get('/titles/:letter', (req, res) => {
     });
 });
 
+app.get('/film/:title', (req, res) => {
+    let title = req.params.title.toLowerCase();
+    console.log(title);
+
+    let query1 = '';
+    
+    let p1 = dbSelect(query1, [`${title}%`]);
+    let p2 = fs.promises.readFile(path.join(template, 'movie_ratings.html'), 'utf-8');
+
+    Promise.all([p1,p2]).then((results) => {
+        let response = results[1];
+        let response_body = '';
+        results[0].forEach((entry) => {
+            let title = entry.FILM
+            let formatted_url_extension = title.replace(/[^a-zA-Z0-9\s]/g, '').replace(/\s+/g, '-').toLowerCase();
+            response_body += '<a href="/film/' + formatted_url_extension + '">' + title + '</a>' + '<br>';
+        });
+        response = response.replace('$$MOVIE TITLES$$', response_body);
+        res.status(200).type('html').send(response);
+    }).catch((error) => {
+        res.status(200).type('txt').send('File not found');
+    });
+});
+
 app.listen(port, () => {
     console.log('Now listening on port ' + port);
 });
